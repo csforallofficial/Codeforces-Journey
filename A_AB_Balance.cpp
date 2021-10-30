096ac5b8771d291a5b21fd5c0cd57930a427f183
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-10-30 16:48:56 +0530

    Add files via upload

---
+++ A_AB_Balance.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        string s;
+        cin >> s;
+        if(s[0]!=s[s.size()-1])
+            s[0] = s[s.size()-1];
+        cout << s << endl;
+    }
+    return 0;
+}
